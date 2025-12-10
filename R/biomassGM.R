aaf633826c46b790a6f42ac3c8ee04d58536a41c
Ian Eddy <ianmseddy@gmail.com>
2025-12-10 13:57:03 -0800

    work in progress

--- R/biomassGM.R
+++ R/biomassGM.R
@@ -28,6 +28,37 @@ calculateClimateEffect <- function(cohortData, pixelGroupMap, cceArgs, year,
                                    cohortDefinitionCols = c("age", "speciesCode", "pixelGroup")) {
 
    # extract relevant args
+
+  #assume that climate normals are present in cceArgs as cceArts$historicalClimate$historical_<var>_normal
+  #assume climate variables are denoted by cceArgs$climateVariables
+  #
+  anomalyVariables <- setdiff(names(climateVariables), "")
+  allClimVar <- c(climateVariables, anomalyVariables)
+
+  #get climate variables for pixelGroupMap
+
+  if (length(anomalies) > 0) {
+    #check if year is already subset - if not, subset to PSP period
+    anomalyVar <-
+    setnames(anomalyData, old = anomalies, new = names(anomalies))
+    PSPmodelData <- anomalyData[PSPmodelData, on = c("OrigPlotID1")]
+
+    #recalculate the anomaly(s) via subtraction
+    for (i in 1:length(anomalies)) {
+      #index because the name isn't preserved if you take the object itself
+      temp <- anomalies[i]
+      setnames(PSPmodelData, c(temp, names(temp)), c("var", "anom"))
+      PSPmodelData[, anom := var - anom]
+      newOrderCols <- names(PSPmodelData)[!names(PSPmodelData) %in% c("var", "anom") ]
+      setcolorder(PSPmodelData, newOrderCols)
+      setnames(PSPmodelData, c("var", "anom"), c(temp, names(temp)))
+    }
+  }
+
+
+
+
+
   ATA <- cceArgs$projectedClimateRasters[["ATA"]][[paste0("year", year)]]
   CMI <- cceArgs$projectedClimateRasters[["CMI"]][[paste0("year", year)]]
   CMInormal <- cceArgs$historicalClimateRasters[["CMI_normal"]]
@@ -153,175 +184,4 @@ calculateClimateEffect <- function(cohortData, pixelGroupMap, cceArgs, year,
   climateEffect[, c("growthPred", "mortPred") := .(asInteger(growthPred), asInteger(mortPred))]
 
   return(climateEffect)
-}
-
-#'  own
-#'
-#' for predicting from gamlss with no random effect
-#'
-#' @param fixed the fixed terms
-#' @param random the random terms
-#' @param correlation this is the correlation structure?
-#' @param method TODO: Description needed
-#' @param level the marginal or conditional predictor
-#' @param ... additional arguments passed to lmeCcontrol
-#' @importFrom nlme lmeControl
-#' @importFrom utils tail
-#' @importFrom stats as.formula fitted formula model.frame predict
-#' @importFrom reproducible .grepSysCalls
-
-#' @rdname own
-#' @export
-own <- function(fixed = ~1, random = NULL, correlation = NULL, method = "ML", level = NULL, ...) {
-  #------------------------------------------
-  # function starts here
-  #------------------------------------------
-  scall <- deparse(sys.call(), width.cutoff = 500L) #
-  if (!is(fixed, "formula")) stop("fixed argument in lme() needs a formula starting with ~")
-  # if (!is(random, "formula")) stop("formula argument in lme() needs a formula starting with ~")
-  # we have to do somehing with corelation
-  # if (!is.null(correlation)) {
-  #   cor.for <- attr(correlation, "formula")
-  #   if (!is.null(cor.for))
-  #     cor.vars <- all.vars(cor.for)
-  # }
-  # else cor.vars <- NULL
-  # get where "gamlss" is in system call
-  # it can be in gamlss() or predict.gamlss()
-
-  # use `tail` because there may be more than one gamlss, e.g., with system.call(gamlss).
-  #  take the last one ... thus tail(..., 1)
-  position <- tail(reproducible::.grepSysCalls(sys.calls(), "gamlss"), 1)
-  # for (i in length(rexpr):1)
-  # {
-  #   position <- i # get the position
-  #   if (rexpr[i]==TRUE) break
-  # }
-  #
-  gamlss.env <- sys.frame(position) # gamlss or predict.gamlss
-  ## --- get the lme control values
-  control <- lmeControl(...)
-  ## get the data
-  if (sys.call(position)[1] == "predict.gamlss()") { # if predict is used
-    Data <- get("data", envir = gamlss.env)
-  } else if (sys.call(position)[1] == "gamlss()") { # if gamlss() is used
-    if (is.null(get("gamlsscall", envir = gamlss.env)$data)) { # if no data argument but the formula can be interpreted
-      Data <- model.frame(formula)
-    } else { # data argument in gamlss
-      Data <- get("gamlsscall", envir = gamlss.env)$data
-    }
-  } else {
-    Data <- get("data", envir = gamlss.env)
-  }
-  Data <- if (any(attributes(eval(substitute(Data)))$class == "groupedData")) {
-    eval(substitute(Data))
-  } else {
-    data.frame(eval(substitute(Data)))
-  }
-  # =====
-  len <- dim(Data)[1] # get the lenth of the data
-  ## out
-  xvar <- rep(0, len) # model.matrix(as.formula(paste(paste("~",ff, sep=""), "-1", sep="")), data=Data) #
-  attr(xvar, "fixed") <- fixed
-  attr(xvar, "random") <- random
-  attr(xvar, "method") <- method
-  attr(xvar, "correlation") <- correlation
-  attr(xvar, "level") <- level
-  attr(xvar, "control") <- control
-  attr(xvar, "gamlss.env") <- gamlss.env
-  if (any(attributes(Data)$class == "groupedData")) {
-    attr(xvar, "data") <- Data
-  } else {
-    attr(xvar, "data") <- as.data.frame(Data)
-  }
-  attr(xvar, "call") <- substitute(gamlss.own(data[[scall]], z, w, ...))
-  attr(xvar, "class") <- "smooth"
-  xvar
-}
-
-#' gamlss.own
-#'
-#' the definition of the backfitting additive function
-#'
-#' @author Mikis Stasinopoulos and Marco Enea
-#'
-#' @param x description missing
-#' @param y description missing
-#' @param w description missing
-#' @param xeval description missing
-#' @importFrom nlme lme varFixed
-#' @importFrom stats as.formula fitted formula model.frame predict
-#' @rdname gamlss.own
-#' @export
-gamlss.own <- function(x, y, w, xeval = NULL) {
-  fixed <- attr(x, "fixed")
-  random <- attr(x, "random")
-  correlation <- attr(x, "correlation")
-  method <- attr(x, "method")
-  level <- attr(x, "level")
-  fix.formula <-
-    as.formula(paste("Y.var", deparse(fixed, width.cutoff = 500L), sep = ""))
-  control <- as.list(attr(x, "control"))
-  # gamlss.env <- as.environment(attr(x, "gamlss.env"))
-  OData <- attr(x, "data")
-  Data <- if (is.null(xeval)) {
-    OData
-  } # the trick is for prediction
-  else {
-    OData[seq(1, length(y)), ]
-  }
-  if (any(attributes(Data)$class == "groupedData")) {
-    Data$W.var <- 1 / w
-    Data$Y.var <- y
-  } else {
-    Y.var <- y
-    W.var <- 1 / w
-    Data <- data.frame(eval(substitute(Data)), Y.var, W.var)
-  }
-  #       Data <- data.frame(eval(substitute(Data)),y,wei=1/w)
-  # fit  <-  lme(All$fixed, data = Data, random=All$random, weights=varFixed(~wei),  method="ML")
-  #
-  #              (fixed, data = sys.frame(sys.parent()), random, correlation = NULL,
-  #           weights = NULL, subset, method = c("REML", "ML"), na.action = na.fail,
-  #           control = list(), contrasts = NULL, keep.data = TRUE)
-  # lme(fixed = fixed, random = random, data = data,
-  #    correlation = correlation, control = control, weights = varFixed(w.formula),
-  #    method = "ML", ...)
-  fit <- lme(
-    fixed = fix.formula,
-    data = Data,
-    random = random,
-    weights = varFixed(~W.var),
-    correlation = correlation,
-    control = control,
-    method = method
-  )
-  fv <- fitted(fit)
-  residuals <- y - fv
-  N <- sum(w != 0)
-  df <- N - (sum(w * (y - fv)^2)) / (fit$sigma^2)
-  if (is.null(xeval)) {
-    list(
-      fitted.values = fv,
-      residuals = residuals,
-      nl.df = df - 1,
-      lambda = fit$sigma,
-      # Mikis 10-6-19 df should be df-1 not df
-      coefSmo = fit,
-      var = NA
-    ) # var=fv has to fixed
-  } else {
-    # ll<-dim(OData)[1]
-    # assign("fix.formula",fix.formula,envir=globalenv())
-    # on.exit(rm(fix.formula,envir=globalenv()))
-    # pred <- eval(expression(predict(fit,newdata = OData[seq(length(y)+1,ll),])),envir=environment() )
-    fit$call$fixed <- substitute(fix.formula)
-    ll <- dim(OData)[1]
-    pred <-
-      if (is.null(level)) {
-        predict(fit, newdata = OData[seq(length(y) + 1, ll), ])
-      } else {
-        predict(fit, newdata = OData[seq(length(y) + 1, ll), ], level = level)
-      }
-  }
 }
